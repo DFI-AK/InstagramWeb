520ecd2660baa62580e3cb82fd9780bcb9360caa
Anassaifi10 <saifianas771@gmail.com>
2024-12-12 14:25:27 +0530

    login signUp Pages

--- InstagramWeb/src/Web/ClientApp/src/app/core/modules/user/login/login.component.ts
+++ InstagramWeb/src/Web/ClientApp/src/app/core/modules/user/login/login.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AccountClient, LoginRequest } from 'src/app/web-api-client';
 
 @Component({
   selector: 'app-login',
@@ -8,5 +10,32 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  private accountClient = inject(AccountClient)
+
+  public loginForm: FormGroup;
+  submitted = false;
+
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]]
+    });
+  }
+
+
+  loginUser() {
+    console.log(this.loginForm)
+    if (this.loginForm.valid) {
+      const loginRequest = new LoginRequest()
+      loginRequest.email = this.loginForm.get('email')?.value
+      loginRequest.password = this.loginForm.get('password')?.value
+      this.accountClient.postApiAccountLogin(null, null, loginRequest)
+        .subscribe({
+          next: token => {
+            console.log(token.accessToken);
+          }
+        })
+    }
+  }
 
 }
--- InstagramWeb/src/Web/ClientApp/src/app/core/modules/user/user.module.ts
+++ InstagramWeb/src/Web/ClientApp/src/app/core/modules/user/user.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { UserRoutingModule } from './user-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -9,6 +10,8 @@ import { UserRoutingModule } from './user-routing.module';
   imports: [
     CommonModule,
     UserRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class UserModule { }
